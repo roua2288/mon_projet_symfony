959d1f53c8cd8d099f4f5a99ba94d1d6afc11ddc
roua <rouabenamor228@email.com>
2025-12-01 18:40:47 +0100

    feat: implémentation de l'authentification Symfony

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,18 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-final class SecurityController extends AbstractController
+class SecurityController extends AbstractController
 {
     #[Route('/security', name: 'app_security')]
-    public function index(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        // Si l'utilisateur est déjà connecté, rediriger vers admin
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        // Récupérer l'erreur de connexion s'il y en a une
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Dernier nom d'utilisateur entré
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
-}
 
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Symfony interceptera cette route
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
 
 
